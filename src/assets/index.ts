55942f9fa347577a71e966b822e40a709feb7ac2
Monica Henao <monicahenao2211@gmail.com>
2023-12-08 10:36:08 -0500

    Update style and image

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -9,11 +9,11 @@ import LogoDarks from "./images/logo-dark.svg";
 import Logop from "./images/logo.png";
 import Logos from "./images/logo.svg";
 import Sample400 from "./tmp/sample-400x600.jpg";
-import Sample540 from "./tmp/sample-540x540.jpg";
-import Sample540alt from "./tmp/sample-540x540-alt.jpg";
-import SampleDigital from "./tmp/sample-digital-map.png";
-import SampleFeaturedVideo from "./tmp/sample-featured-video.jpg";
-import SampleFeatured from "./tmp/sample-featured.jpg";
+import Sample540 from "./tmp/sample-400x600.jpg";
+import Sample540alt from "./tmp/sample-400x600.jpg";
+import SampleDigital from "./tmp/sample-400x600.jpg";
+import SampleFeaturedVideo from "./tmp/sample-400x600.jpg";
+import SampleFeatured from "./tmp/sample-400x600.jpg";
 
 export {
   LogoDarkp,
